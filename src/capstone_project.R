e6b5c46c95d175f3172b767b9dde9ac25d99b3b2
Leo Wong <leo.wong2@mail.mcgill.ca>
2019-03-13 16:40:03 -0400

    Add additional features and format code

--- src/capstone_project.R
+++ src/capstone_project.R
@@ -1,8 +1,8 @@
-####################################################################################################
-######################                                                        ######################
-######################                      CAPSTONE PROJECT                  ######################
-######################                                                        ######################
-####################################################################################################
+############################################################################################
+##################                                                        ##################
+##################                      CAPSTONE PROJECT                  ##################
+##################                                                        ##################
+############################################################################################
 
 # Set working directory
 setwd("~/GitHub/YCBS299/capstone_project")
@@ -28,66 +28,61 @@ df_clean <- calls_raw
 # Rename variables
 names(df_clean)[names(df_clean) %in% c("Date.Time", "Mean.Temp...C.", "Total.Rain..mm.", "Total.Snow..cm.")] <- c("date_time", "temp_mean", "rain_total", "snow_total")
 
-# Derive variables of interest (potential features)
-df_clean <- within(df_clean, {
-  
-  season <- getSeason(as.Date(date, "%Y-%m-%d"))
-  day_of_week <- gsub("0", "Sunday", day_of_week)
-  day_of_week <- gsub("1", "Monday", day_of_week)
-  day_of_week <- gsub("2", "Tuesday", day_of_week)
-  day_of_week <- gsub("3", "Wednesday", day_of_week)
-  day_of_week <- gsub("4", "Thursday", day_of_week)
-  day_of_week <- gsub("5", "Friday", day_of_week)
-  day_of_week <- gsub("6", "Saturday", day_of_week)
-  is_holiday <- as.logical(is_holiday)
-  # temp_mean <- as.numeric(scale(temp_mean))
-  # rain_total <- as.numeric(scale(rain_total))
-  # snow_total <- as.numeric(scale(snow_total))
-  
-})
 
 #########################
 ### DATA MANIPULATION ###
 #########################
 
 # Aggregate data so that every row represent a day with the sum of calls.
-# Note the the additional variables are features that a specific to an overall day.
+# Note the the additional variables are features that are specific to a given day.
 # (ie. cannot use neighborhood, since within a day there could be many).
 
+# Aggreagate by unique day (year-month-day) and count number of calls
 df_callsperday <- aggregate(data = df_clean, nom_ville ~ year + month + day, FUN = length)
 
+# Merge predictors back to aggregated dataset (features of each day)
 df_callsperday <- mergePredictors(df = df_callsperday,
                                   df_raw = "df_clean",
-                                  vars = c("temp_mean", "rain_total", "snow_total"), 
+                                  vars = c("temp_mean", "did_rain", "did_snow", 
+                                           "is_holiday", "day_of_week"),
                                   merge_by = c("year", "month", "day"))
 
+# Rename variable
 names(df_callsperday)[names(df_callsperday) == "nom_ville"] <- "calls_count"
 
+# Derive and format predictors
 df_callsperday <- within(df_callsperday, {
   
-  calls_count <- round(calls_count)
-  month <- as.character(month)
+  month <- as.factor(month)
+  did_rain <- as.factor(did_rain)
+  did_snow <- as.factor(did_snow)
+  is_holiday <- as.factor(is_holiday)
+  day_of_week <- gsub("0", "Monday", day_of_week)
+  day_of_week <- gsub("1", "Tuesday", day_of_week)
+  day_of_week <- gsub("2", "Wednesday", day_of_week)
+  day_of_week <- gsub("3", "Thursday", day_of_week)
+  day_of_week <- gsub("4", "Friday", day_of_week)
+  day_of_week <- gsub("5", "Saturday", day_of_week)
+  day_of_week <- gsub("6", "Sunday", day_of_week)
+  day_of_week <- as.factor(day_of_week)
+  year <- as.factor(year)
+  is_pre <- ifelse(as.numeric(as.character(year)) <= 2009, 1, 0)
+  is_pre <- as.factor(is_pre)
   
 })
 
-df_callsperday <- subset(df_callsperday, !is.na(temp_mean) & !is.na(rain_total) & !is.na(snow_total))
+# Remove records where any of the following predictors are missing
+df_callsperday <- subset(df_callsperday, !is.na(temp_mean) & !is.na(did_rain) & !is.na(did_snow) & !is.na(is_holiday))
 
-# Define training dataset as any data points between 2010 and 2017. 
-# The rationale is that there was a spike in number of calls in 2008 when the roles
-# of fire fighters expanded to first response.
-df_train <- subset(df_callsperday, year > 2009 & year < 2018)
-# Define testing dataset as any data points after 2018.
-df_test <- subset(df_callsperday, year == 2018)
-
-# 
-# df_train_mean <- aggregate(data = df_train, calls_count ~ month + day, FUN = mean)
-# 
-# df_train_mean <- mergePredictors(df = df_train_mean, 
-#                                  df_raw = "df_train",
-#                                  vars = c("temp_mean", "rain_total", "snow_total"), 
-#                                  merge_by = c("month", "day"))
-# 
-# df_train_mean$calls_count <- round(df_train_mean$calls_count)
+# Define train and test sets (80/20 split)
+set.seed(101)
+# Percentage of split set to 80
+bound <- floor((nrow(df_callsperday)/5)*4)
+# Sample rows
+df_callsperday <- df_callsperday[sample(nrow(df_callsperday)), ]
+# Assign train and test data
+df_train <- df_callsperday[1:bound, ]
+df_test <- df_callsperday[(bound+1):nrow(df_callsperday), ]
 
 ################
 ### MODELING ###
@@ -95,25 +90,29 @@ df_test <- subset(df_callsperday, year == 2018)
 
 outcome = "calls_count"
 # vars = c("day_of_week", "is_holiday", "temp_mean", "rain_total", "snow_total", "month")
-vars = c("month", "temp_mean", "rain_total", "snow_total")
+vars = c("year", "month", "temp_mean", "did_rain", "did_snow", "is_holiday", "day_of_week")
 
 # Compute the mean and variance to see if there is a significant difference
 mean(df_train$calls_count)
 var(df_train$calls_count)
-# mean(df_train_mean$calls_count)
-# var(df_train_mean$calls_count)
 
 # Define the Y and x
 fmla <- paste(outcome, "~", paste(vars, collapse = " + "))
 
+##########################
+### GLM - QUASIPOISSON ###
+##########################
+
 # Run model (Note we use quasipoisson since var >>> mean)
 po_model <- glm(fmla, data = df_train, family = quasipoisson)
-# po_model_mean <- glm(fmla, data = df_train_mean, family = poisson)
 
+# Calculate pseudo R2 (the closer to 1, the better)
 pseudoR2 <- 1 - po_model$deviance/po_model$null.deviance
 pseudoR2
-# pseudoR2_mean <- 1 - po_model_mean$deviance/po_model_mean$null.deviance
-# pseudoR2_mean
+
+#####################
+### RANDOM FOREST ###
+#####################
 
 # Test prediction
 library(dplyr)
@@ -121,15 +120,11 @@ df_test_po <- df_test
 # df_test_po_mean <- df_test
 df_test_po$pred <- predict(po_model, newdata = df_test_po, type = "response")
 df_test_po %>% mutate(residual = pred - calls_count) %>% summarize(rmse = sqrt(mean(residual^2)))
-# df_test_po_mean$pred <- predict(po_model_mean, newdata = df_test_po, type = "response")
-# df_test_po_mean %>% mutate(residual = pred - calls_count) %>% summarize(rmse = sqrt(mean(residual^2)))
 
-# NEXT STEPS:
-# 1) Compare models using different features (AIC).
-# 2) Split data into train and test and minimize MSE.
-# 3) Try different models (ie. Random Forest).
 
-# Gradient boosting
+#########################
+### GRADIENT BOOSTING ###
+#########################
 
 library(ranger)
 library(dplyr)
@@ -170,17 +165,6 @@ newvars <- treatplan$scoreFrame %>% filter(code %in% c("clean", "lev")) %>% magr
 
 df_md_gb_test.treat <- prepare(treatplan, df_md_gb_test, varRestriction = newvars)
 
-df_md_gb_test.treat$month_lev_x_4 <- 0
-df_md_gb_test.treat$month_lev_x_5 <- 0
-df_md_gb_test.treat$month_lev_x_6 <- 0
-df_md_gb_test.treat$month_lev_x_7 <- 0
-df_md_gb_test.treat$month_lev_x_8 <- 0
-df_md_gb_test.treat$month_lev_x_9 <- 0
-df_md_gb_test.treat$month_lev_x_10 <- 0
-df_md_gb_test.treat$month_lev_x_11 <- 0
-df_md_gb_test.treat$month_lev_x_12 <- 0
-
-names(df_md_gb_test.treat) <- names(df_md_gb.treat)
 
 
 df_md_gb_test.treat$pred <- predict(gb_model, as.matrix(df_md_gb_test.treat))
@@ -196,7 +180,3 @@ df_md_gb_test %>% mutate(residual = gb_pred - calls_count) %>% summarize(rmse = 
 plot(x=df_md_gb_test$gb_pred,y=df_md_gb_test$calls_count)
 abline(lm(df_md_gb_test$calls_count ~ df_md_gb_test$gb_pred), col = "blue")
 
-
-# Add additional features (day of week, is holiday, is_before_2008)
-# Change continuous to categorical (rain, snow)
-# Properly split data 20/80
